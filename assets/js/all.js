162e5342f9aa4b73ccfcdefa53fcc4d5e7883d72
hsinyun <ruby8941@gmail.com>
2021-08-23 23:33:43 +0800

    Update 2021-08-23T15:33:26.131Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,10 +3,23 @@
 $(document).ready(function () {
   var pathname = window.location.pathname;
   $('.nav-bar__list-item > .nav-bar__item-link[href="' + pathname + '"]').parent().addClass('active');
+  console.log(pathname, 'pathname');
   $('.slide-content').hide();
   $('.slide-button').click(function () {
     $('.slide-button__icon').toggleClass('active');
     $('.slide-content').slideToggle();
+  });
+  $('.open-reply-btn').click(function () {
+    $('.assignmant-comment__reply').addClass('active');
+    $('.reply-answer-js').addClass('hide');
+    $('.reply-btns').addClass('hide');
+    $('.ask-content').removeClass('has-border');
+  });
+  $('.close-reply-btn').click(function () {
+    $('.assignmant-comment__reply').removeClass('active');
+    $('.reply-answer-js').removeClass('hide');
+    $('.reply-btns').removeClass('hide');
+    $('.ask-content').addClass('has-border');
   });
 });
 //# sourceMappingURL=all.js.map
